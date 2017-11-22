f8199d6d4e0756cad8492b0ab5baa316c044cbd4
daemon369 <daemon336699@gmail.com>
2017-11-22 11:13:03 +0800

    parse address

--- address/address.go
+++ address/address.go
@@ -1,5 +1,10 @@
 package address
 
+import (
+	"net"
+	"errors"
+)
+
 const (
 	Unknown = 0x00
 	IPv4    = 0x01
@@ -9,4 +14,28 @@ const (
 
 func Support(addressType byte) bool {
 	return IPv4 == addressType || FQDN == addressType || IPv6 == addressType
+}
+
+func ParseAddress(addr string) (addrType int, host string, ip net.IP, err error) {
+	addrType = Unknown
+
+	if ip = net.ParseIP(addr); ip != nil {
+		if ip4 := ip.To4(); ip4 != nil {
+			addrType = IPv4
+			ip = ip4
+		} else {
+			addrType = IPv6
+		}
+
+	} else {
+		host = addr
+
+		if len(host) <= 0 || len(host) > 255 {
+			return Unknown, host, nil, errors.New("host name length illegal")
+		}
+
+		addrType = FQDN
+	}
+
+	return addrType, host, ip, err
 }
---
+++ address/address_test.go
@@ -0,0 +1,58 @@
+package address
+
+import (
+	"testing"
+	"fmt"
+	"net"
+)
+
+func Test_parse(t *testing.T) {
+
+	type testCase struct {
+		host   string
+		expect bool
+	}
+
+	var addrList []testCase
+	addrList = append(addrList, testCase{"www.baidu.com", true})
+	addrList = append(addrList, testCase{"1.2.3.4", true})
+	addrList = append(addrList, testCase{"::ffff:c000:0280", true})
+	addrList = append(addrList, testCase{"9999", false})
+	addrList = append(addrList, testCase{"www.meizu.com", true})
+	addrList = append(addrList, testCase{"localhost", true})
+
+	for _, v := range addrList {
+		addrType, host, ip, err := ParseAddress(v.host)
+
+		fmt.Println(addrType, host, ip, err)
+
+		var ret = false
+
+		if err == nil {
+			switch addrType {
+			case IPv4:
+				ip4 := ip.To4()
+				ret = ip4 != nil && len(ip4) == net.IPv4len
+
+			case FQDN:
+				ret = len(host) > 0 && len(host) < 256
+
+			case IPv6:
+				ip6 := ip.To16()
+				ret = ip6 != nil && len(ip6) == net.IPv6len
+
+			default:
+				ret = false
+			}
+		}
+
+		if ret == v.expect {
+			t.Log("success")
+		} else {
+			t.Error("failed")
+		}
+
+		fmt.Println()
+	}
+
+}
