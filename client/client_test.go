d9c4224e0bb0b12a5c3b741fcc874269b46c4534
daemon369 <daemon336699@gmail.com>
2017-12-02 19:47:29 +0800

    client test

--- client/client_test.go
+++ client/client_test.go
@@ -1,16 +1,15 @@
 package client
 
 import (
+	"os"
 	"testing"
 	"github.com/daemon369/go-socks5/auth/noauth"
 	"github.com/daemon369/go-socks5/address"
-	"os"
-	"io"
 )
 
 func Test_client(t *testing.T) {
 	c := &Client{ProxyAddr: "127.0.0.1:1080", Authenticator: noauth.New()}
-	addr := &address.Address{Type: address.FQDN, Host: "www.baidu.com", Port: 443}
+	addr := &address.Address{Type: address.FQDN, Host: "www.baidu.com", Port: 80}
 	conn, err := c.Connect(addr)
 
 	if err != nil {
@@ -19,6 +18,13 @@ func Test_client(t *testing.T) {
 	}
 
 	defer conn.Close()
+
+	get := "GET / HTTP/1.1\n\n"
+
+	if _, err = conn.Write([]byte(get)); err != nil {
+		t.Error(err)
+		return
+	}
 
 	file, err := os.Create("baidu.html")
 	if err != nil {
@@ -30,7 +36,7 @@ func Test_client(t *testing.T) {
 
 	buf := make([]byte, 4096)
 
-	n, err := io.ReadFull(conn, buf)
+	n, err := conn.Read(buf)
 
 	if err != nil {
 		t.Error(err)
